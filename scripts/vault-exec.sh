746a85c8f0881a5c275b069d9dae30f72180a0c7
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-10-30 15:05:12 +0100

    fix(vault exec script): move params to vars to allow complicated commands

--- scripts/vault-exec.sh
+++ scripts/vault-exec.sh
@@ -3,4 +3,4 @@
 #§ 'VAULT_ADDR="' + data.config.vault.url + '"'
 VAULT_ADDR="https://127.0.0.1:8200"
 
-docker run --net=host --cap-add IPC_LOCK -it vault "$@" -address="${VAULT_ADDR}" -tls-skip-verify
+docker run --net=host -e 'VAULT_SKIP_VERIFY=1' -e "VAULT_ADDR=${VAULT_ADDR}" --cap-add IPC_LOCK -it vault "$@"
